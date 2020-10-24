f7aca72421684cdfc17f93cb8e225741c5106426
Stéphane Depierrepont <toorop@toorop.fr>
2020-10-24 18:07:31 +0200

    2 encoders

--- src/arduino/src/main.cpp
+++ src/arduino/src/main.cpp
@@ -4,16 +4,22 @@
 #include "Encoder.h"
 
 void tickE1();
+void tickE2();
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Définition des PIN physique //////////////////////////////////////////////////////////////////////////////////
 //
 
-// Encoder A ou 1 (Connecter +5v et GND)
-#define ROT_A_DT 24
-#define ROT_A_CLK 22
-#define ROT_A_SW 26
+// Encoder 1 (E1) 
+#define ROT_E1_CLK 22
+#define ROT_E1_DT 23
+#define ROT_E1_SW 24
 
+// Encoder 2 (E2)
+#define ROT_E2_CLK 25
+#define ROT_E2_DT 26
+#define ROT_E2_SW 27
 
 /// https://roboticsbackend.com/arduino-object-oriented-programming-oop/
 
@@ -25,8 +31,8 @@ void tickE1();
 //
 
 // encodeurs rotatifs
-Encoder e1("E1", ROT_A_DT, ROT_A_CLK);
-//Encoder e2("E2", ROT_B_DT, ROT_B_CLK);
+Encoder e1("E1", ROT_E1_DT, ROT_E1_CLK);
+Encoder e2("E2", ROT_E2_DT, ROT_E2_CLK);
 
 void setup()
 {
@@ -34,8 +40,8 @@ void setup()
 
   // encoderA et encoderB sont des proxy pour lancer la fonction `changement` sur les instances
   // on ne peut pas attaquer ces methodes directement ici (limitation de attachInterrupt sur arduino)
-  attachInterrupt(digitalPinToInterrupt(e1.getPinDT()), tickE1, CHANGE);
-  //attachInterrupt(digitalPinToInterrupt(e2.getPinDT()), changementE2, CHANGE);
+   attachInterrupt(digitalPinToInterrupt(e1.getPinDT()), tickE1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(e2.getPinDT()), tickE2, CHANGE);
 }
 
 void loop()
@@ -48,7 +54,8 @@ void tickE1()
   e1.tick();
 }
 
-// Proxy pour e2.changement
-/*void changementE2(){
-  e2.changement();
-}*/
+void tickE2()
+{
+  e2.tick();
+}
+
